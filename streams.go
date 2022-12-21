17b5382909de89a91135712f44462cfa3561816a
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2022-12-20 17:12:24 -0800

    add StreamManager

---
+++ streams.go
@@ -0,0 +1,37 @@
+package grpcboot
+
+import (
+	"context"
+	"sync"
+
+	"google.golang.org/grpc"
+)
+
+type StreamManager[T any] struct {
+	streams []grpc.ServerStream
+	lock    sync.Mutex
+	Context context.Context
+}
+
+func NewStreamManager[T any](ctx context.Context) *StreamManager[T] {
+	return &StreamManager[T]{
+		lock:    sync.Mutex{},
+		Context: ctx,
+	}
+}
+
+func (m *StreamManager[T]) Add(stream grpc.ServerStream) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	m.streams = append(m.streams, stream)
+}
+
+func (m *StreamManager[T]) Send(value T) {
+	for _, s := range m.streams {
+		s.SendMsg(value)
+	}
+}
+
+func (m *StreamManager[T]) Wait() {
+	<-m.Context.Done()
+}
